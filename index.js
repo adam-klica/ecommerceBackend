76193d44ca409c2446db38cc2b9ba249962183b4
Lema <lemi.klica@gmail.com>
2025-11-28 23:26:33 +0100

    asd

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const { secret } = require("./config/secret");
 const PORT = secret.port || 7000;
-const morgan = require('morgan')
+const morgan = require("morgan");
 // error handler
 const globalErrorHandler = require("./middleware/global-error-handler");
 // routes
@@ -25,23 +25,23 @@ const downloadRoutes = require("./routes/download.routes");
 // middleware
 app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
 
 // connect database
 connectDB();
 
-app.use("/api/user", userRoutes);
-app.use("/api/category", categoryRoutes);
-app.use("/api/brand", brandRoutes);
-app.use("/api/product", productRoutes);
+app.use("/user", userRoutes);
+app.use("/category", categoryRoutes);
+app.use("/brand", brandRoutes);
+app.use("/product", productRoutes);
 // app.use('/api/upload',uploadRouter);
-app.use("/api/order", orderRoutes);
-app.use("/api/user-order", userOrderRoutes);
-app.use("/api/review", reviewRoutes);
-app.use("/api/cloudinary", cloudinaryRoutes);
-app.use("/api/admin", adminRoutes);
-app.use("/api/download", downloadRoutes);
+app.use("/order", orderRoutes);
+app.use("/user-order", userOrderRoutes);
+app.use("/review", reviewRoutes);
+app.use("/cloudinary", cloudinaryRoutes);
+app.use("/admin", adminRoutes);
+app.use("/download", downloadRoutes);
 
 // root route
 app.get("/", (req, res) => res.send("Apps worked successfully"));
@@ -54,11 +54,11 @@ app.use(globalErrorHandler);
 app.use((req, res, next) => {
   res.status(404).json({
     success: false,
-    message: 'Not Found',
+    message: "Not Found",
     errorMessages: [
       {
         path: req.originalUrl,
-        message: 'API Not Found',
+        message: "API Not Found",
       },
     ],
   });
